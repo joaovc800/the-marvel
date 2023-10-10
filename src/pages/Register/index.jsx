82769ea8e4d6b2c015f92d95207c557f93009375
RobsonHB <robson.h.belinho@gmail.com>
2023-10-10 04:40:54 -0300

    cadastro

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -84,58 +84,15 @@ export function Register() {
               </Form>
               </Col>
               
-
-
-
-              
-
             </Card.Body>
           </Card>
-          ))}
-          
+          ))}   
             
             
         </Col>
         <Col sm={8} className="bg-img2"></Col>
       </Row>
     </div>
-    // <div className="container">
-    //   <header className="header">
-    //     <img src={logoImg} alt="Workflow" className="logoImg" />
-    //     <span>Por favor digite suas informações de cadastro</span>
-    //   </header>
-
-    //   <form>
-    //     <div className="inputContainer">
-    //       <label htmlFor="email">E-mail</label>
-    //       <input
-    //         type="text"
-    //         name="email"
-    //         id="email"
-    //         placeholder="johndoe@gmail.com"
-    //         onChange={(e) => setEmail(e.target.value)}
-    //       />
-    //     </div>
-
-    //     <div className="inputContainer">
-    //       <label htmlFor="password">Senha</label>
-    //       <input
-    //         type="password"
-    //         name="password"
-    //         id="password"
-    //         placeholder="********************"
-    //         onChange={(e) => setPassword(e.target.value)}
-    //       />
-    //     </div>
-
-    //     <button onClick={handleSignOut} className="button">
-    //       Cadastrar <img src={arrowImg} alt="->" />
-    //     </button>
-    //     <div className="footer">
-    //       <p>Você já tem uma conta?</p>
-    //       <Link to="/">Acesse sua conta aqui</Link>
-    //     </div>
-    //   </form>
-    // </div>
+    
   );
 }
