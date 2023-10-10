aba92b3f86bd3bd9f0eb0739a34c9d59b8262658
João Vinicius da Costa <joaovc@uninove.br>
2023-10-10 17:59:32 -0300

    tela de cadastro

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,96 +1,63 @@
 import { useState } from "react";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { Link } from "react-router-dom";
-import arrowImg from "../../assets/arrow.svg";
-import logoImg from "../../assets/logo-marvel-256.png";
 import { auth } from "../../services/firebaseConfig";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup';
 import "./styles.css";
 
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+
+
+const validation = yup.object().shape({
+  username: yup.string().required("Nome é obrigatório"),
+  password: yup.string().required("Link da imagem é obrigatória")
+})
+
 
 export function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const [createUserWithEmailAndPassword, user, loading, error] =
-    useCreateUserWithEmailAndPassword(auth);
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    resolver: yupResolver(validation)
+  });
 
-  function handleSignOut(e) {
-    e.preventDefault();
-    createUserWithEmailAndPassword(email, password);
+  const [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth);
+
+  function handleSignOut(data) {
+    //data.preventDefault()
+    createUserWithEmailAndPassword(data.username, data.password)
+    
   }
 
   if (loading) {
-    return <p>carregando...</p>;
+    return <p>carregando...</p>
   }
+
   return (
-
-    <div className="bg-img2">
-      
-      <Row style={{height: '97vh'}}>
-        <Col sm={4} >
-          {['Dark'].map((variant) => (
-            <Card
-              bg={variant.toLowerCase()}
-              key={variant}
-              text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-              style={{ width: '30rem', height: '97vh'}}
-              className=""
-            >
-
-            <Card.Body>
-              <Col md={{ span: 7, offset: 3 }}>
-              <header className="header">
-                <img src={logoImg} alt="Workflow" className="logoImg" />
-                <div className="row">
-                  <span className="center">Crie o seu login inserindo os dados abaixo</span>
-                </div>
-              </header>
-              <Form /*onSubmit={handleSubmit(onSubmit)}*/>
-                <div className="py-3">
-
-                
-                <Form.Group as={Row} controlId="formPlaintextEmail">
-                  <Form.Label column sm="5">
-                    Email
-                  </Form.Label>
-                
-                    <Form.Control type="text" /*{...register("email")} */ onChange={(e) => setEmail(e.target.value)} placeholder="email@exemplo.com" />
-                    <p className="error-message">{/*errors.email?.message*/}</p>
-                
-                </Form.Group>
-                <Form.Group as={Row} controlId="formPlaintextPassword">
-                  <Form.Label column sm="5">
-                    Senha
-                  </Form.Label>
-                
-                    <Form.Control type="password" /*{...register("password")}*/ onChange={(e) => setPassword(e.target.value)} placeholder="********************" />
-                    <p className="error-message">{/*errors.password?.message*/}</p>
-                
-                </Form.Group>
-                
-                  <Form.Group as={Row} controlId="">
-                    <Button variant="secondary" type="submit">Criar acesso</Button>
-                  </Form.Group>
-                
-                  </div>
-              </Form>
-              </Col>
-              
-            </Card.Body>
-          </Card>
-          ))}   
-            
-            
-        </Col>
-        <Col sm={8} /*className="bg-img2"*/></Col>
-      </Row>
+    <div className="limiter">
+      <div className="container-login100 bg-img2" >
+        <div className="wrap-login100 p-t-30 p-b-50">
+          <span className="bangers fs-1 text-center text-light">
+            Cadastre-se
+          </span>
+          <Form onSubmit={handleSubmit(handleSignOut)} className="login100-form validate-form p-b-33 p-t-5">
+            <div className="d-flex flex-column p-4">
+              <Form.Group className="py-3" controlId="username-form">
+                <Form.Label>Usuário</Form.Label>
+                <Form.Control type="text" {...register("username")} name="username" placeholder="name@example.com"></Form.Control>
+              </Form.Group>
+              <Form.Group className="py-3" controlId="password-form">
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type="password" {...register("password")} name="password" placeholder="******"></Form.Control>
+              </Form.Group>
+              <Button type="submit" className="mt-4 btn btn-primary">
+                Criar usuário
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </div>
     </div>
-    
-  );
+  )
 }
